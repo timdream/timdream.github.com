8741a3d7f5167a2603204c85ff0e2af34a302108
Timothy Guan-tin Chien <timdream@gmail.com>
2011-09-24 00:42:25 +0800

    rename manifest to appcache

--- update_cache.sh
+++ update_cache.sh
@@ -2,5 +2,5 @@
 
 # Update hash value in cache.manifest by generate md5 hash of all local files
 
-HASH=`find . -type f | grep -v \.git | grep -v "cache\.manifest" | xargs cat - | md5`
-eval "sed -i '' -E -e 's/^# hash.*$/# hash "$HASH"/' cache.manifest"
+HASH=`find . -type f | grep -v \.git | grep -v "site\.appcache" | xargs cat - | md5`
+eval "sed -i '' -E -e 's/^# hash.*$/# hash "$HASH"/' site.appcache"
