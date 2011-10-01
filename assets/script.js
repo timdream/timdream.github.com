6f0eca03a8ef04c4b36884594a6f473a647e034c
Timothy Guan-tin Chien <timdream@gmail.com>
2011-10-01 19:33:56 +0800

    updated version

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,28 @@
 jQuery(function ($) {
-	$('#contact a').bind(
+
+	function handleHash (hash) {
+		$('body > section').hide();
+		window.location.hash = hash;
+		$(hash).show();
+	}
+
+	$('.contact a').bind(
 		'click',
 		function (ev) {
 			ev.preventDefault();
 			window.open(this.href, '', 'width=480,height=360');
 		}
 	);
+	$('nav a').bind(
+		'click',
+		function (ev) {
+			handleHash($(this).attr('href')); // not using this.hash
+			ev.preventDefault();
+		}
+	);
+	if (window.location.hash) {
+		handleHash(window.location.hash);
+	} else {
+		handleHash('#about');
+	}
 });
