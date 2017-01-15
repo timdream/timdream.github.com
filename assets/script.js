8238c251eaf5897e945dd091f09dba6a74eeb70c
Timothy Guan-tin Chien <timdream@gmail.com>
2017-01-15 21:29:56 +0800

    Remove jQuery

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
+'use strict';
 
-jQuery(function docReady($) {
-
-  var reloadOnNextHashChange = false;
-
-  function handleHash() {
+(function(){
+  var handleHash = window.onhashchange = function() {
     var hash = window.location.hash;
 
     // prevent scrolling by hide the element first and use replace()
-    $('body > section').hide();
-    if (hash) window.location.replace(hash);
-    $(hash || '#about').show();
-    if (reloadOnNextHashChange) window.location.reload();
+    var sectionEls = document.querySelectorAll('body > section');
+    var sectionToShow = hash.substr(1) || 'about';
+    var elToShow;
+    Array.prototype.forEach.call(sectionEls, function(el) {
+      el.hidden = true;
+      if (sectionToShow === el.id) {
+        elToShow = el;
+      }
+    });
+    if (hash) {
+      window.location.replace(hash);
+    }
+    elToShow.hidden = false;
   }
+  handleHash();
 
-  $('.contact a').on(
+  document.querySelector('.contact a').addEventListener(
     'click',
     function contactClick(ev) {
       ev.preventDefault();
@@ -21,48 +29,20 @@ jQuery(function docReady($) {
     }
   );
 
-  $('.make').one(
+  document.querySelector('.make').addEventListener(
     'dblclick',
     function makeDblclick(ev) {
+      this.removeEventListener('dblclick', makeDblclick);
       ev.preventDefault();
-      $(this).attr('contenteditable', true).focus();
+      this.setAttribute('contenteditable', true);
+      this.focus();
     }
   );
 
-  if ('onhashchange' in window) {
-    window.onhashchange = handleHash;
-  } else {
-    // no crappy iframe of IE6/7, just make sure $('a') links works
-    $(window).on(
-      'click',
-      'a',
-      function shimmedHashchange(ev) {
-        if (ev.which == 2 || ev.metaKey)
-          return true;
-
-        var $this = $(this);
-
-        if ($this.attr('href').substr(0,1) === '#') {
-          window.location.replace($this.attr('href'));
-          handleHash();
-        }
-      }
-    );
-  }
-  handleHash();
-
-  $.getScript('https://apis.google.com/js/plusone.js');
-
-  if (window.applicationCache) {
-    window.applicationCache.addEventListener(
-      'error', // can't get manifest file - currently offline
-      function appCacheError() {
-        $('html').addClass('applicationcache-offline');
-      }
-    );
-  }
-
-});
+  var scriptEl = document.createElement('script');
+  scriptEl.src = 'https://apis.google.com/js/plusone.js';
+  document.documentElement.firstElementChild.appendChild(scriptEl);
+})();
 
 /* AppCacheUI: https://github.com/timdream/appcacheui */
 (function(){var e={init:function(){var a=document,b=a.body,c=window.applicationCache;if(c)if(b){this.info=a.getElementById("appcache_info");if(!this.info){a.cE=a.createElement;var d=a.cE("a"),a=a.cE("div");a.id="appcache_info";d.href="";a.appendChild(d);b.firstChild&&b.insertBefore(a,b.firstChild);this.info=a}"checking,downloading,progress,noupdate,cached,updateready,obsolete,error".split(",").forEach(function(a){c.addEventListener(a,e)})}else console.log("Premature init. Put the <script> in <body>.")},
