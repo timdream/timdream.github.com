055b7da966d38f51d875052e42cf116a9788bddc
Timothy Guan-tin Chien <timdream@gmail.com>
2017-09-26 01:16:56 +0800

    quotes

--- assets/cv.js
+++ assets/cv.js
@@ -222,10 +222,10 @@ CVDecipher.prototype = {
   load: function(password) {
     var worker;
     if (this.useWebCrypto) {
-      worker = this.worker = new Worker('/assets/webcrypto-decipher.min.js?_=ae158a');
+      worker = this.worker = new Worker('/assets/webcrypto-decipher.min.js?_=272ec7');
       this.decipherName = 'Web Crypto Decipher';
     } else {
-      worker = this.worker = new Worker('/assets/asmcrypto-decipher.min.js?_=189c07');
+      worker = this.worker = new Worker('/assets/asmcrypto-decipher.min.js?_=8f0e15');
       this.decipherName = 'asmCrypto Decipher';
     }
     worker.postMessage({ password: password });
